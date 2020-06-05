cbed1ddeeee2c4e964017316dfe9b998468f9ead
b-hayes <bradozman@gmail.com>
2020-06-06 01:04:18 +1000

    bobing accomplished

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -2,28 +2,18 @@
 
 public class Pickup : MonoBehaviour
 {
-    public float bobAmount = 1f;
-    public float speed = 1f;
-
-    private Vector3 startPosition;
-    private Vector3 endPosition;
-    private float bobDirection = 1f;
-
+    public float bobAmount = 0.002f;
+    public float speed = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        startPosition = transform.position;
-        startPosition = new Vector3(startPosition.x, startPosition.y + bobAmount, startPosition.z);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Mathf.Abs(transform.position.y - startPosition.y) > bobAmount)
-        {
-            bobDirection *= -1f;
-        }
-        transform.Translate(Vector3.up * (Time.deltaTime * speed * bobDirection) );
+        transform.position += Vector3.up * (Mathf.Sin (Time.time * speed) * bobAmount);
     }
 }
