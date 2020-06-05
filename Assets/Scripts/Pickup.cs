1c2c09aa5e0f747cff34552a80c27d749bc24c7d
b-hayes <bradozman@gmail.com>
2020-06-06 05:38:16 +1000

    sound and music test

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Pickup : MonoBehaviour
 {
     public float bobAmount = 0.002f;
     public float bobSpeed = 2f;
-    public float rotationSpeed = 20f;
+    public float rotationSpeed = 50f;
+
+    public AudioClip[] pickUpSounds;
 
     // Start is called before the first frame update
     void Start()
@@ -25,7 +28,29 @@ public class Pickup : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             print("PLayer has picked up an item.");
-            Destroy(gameObject);
+            PlayPickUpSound();
+            // Destroy(gameObject); //todo: stops sound on destroy so..
         }
+    }
+
+    private void PlayPickUpSound()
+    {
+        int length = pickUpSounds.Length;
+        if (length < 1)
+        {
+            print("No pickUp sound to play");
+            return;
+        }
+        AudioClip clip = pickUpSounds[Random.Range(0,length)];
+        AudioSource audioSource = GetComponent<AudioSource>();
+        if (! audioSource)
+        {
+            print("Pickup has no audio source");
+            return;
+        }
+        audioSource.clip = clip;
+        audioSource.loop = false;
+        audioSource.Play();
+        print("Pickup sound played");
     }
 }
