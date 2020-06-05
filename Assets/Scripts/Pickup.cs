f6907b155b457a68701e2caaa25e615832b1889a
b-hayes <bradozman@gmail.com>
2020-06-06 00:53:05 +1000

    wip bobing

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PickupAnimation : MonoBehaviour
+public class Pickup : MonoBehaviour
 {
+    public float bobAmount = 1f;
+    public float speed = 1f;
+
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    private float bobDirection = 1f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;
+        startPosition = new Vector3(startPosition.x, startPosition.y + bobAmount, startPosition.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Mathf.Abs(transform.position.y - startPosition.y) > bobAmount)
+        {
+            bobDirection *= -1f;
+        }
+        transform.Translate(Vector3.up * (Time.deltaTime * speed * bobDirection) );
     }
 }
