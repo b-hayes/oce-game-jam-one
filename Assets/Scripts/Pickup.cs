b30fb399c19bfd06f989e6bf036cf126468e1626
b-hayes <bradozman@gmail.com>
2020-06-06 14:36:19 +1000

    bananas are pickups now with vfx

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -32,33 +32,16 @@ public class Pickup : MonoBehaviour
         {
             print("PLayer has picked up an item.");
             PlayPickUpSound();
-            // Destroy(gameObject); //todo: stops sound on destroy so..
+            Instantiate(pickUpEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
         }
     }
 
     private void PlayPickUpSound()
     {
         int length = pickUpSounds.Length;
-        if (length < 1)
-        {
-            print("No pickUp sound to play");
-            return;
-        }
+        if (length < 1) return;
         AudioClip clip = pickUpSounds[Random.Range(0,length)];
-        // AudioSource audioSource = GetComponent<AudioSource>();
-        
         AudioSource.PlayClipAtPoint(clip, gameObject.transform.position);
-        //
-        //
-        // if (! audioSource)
-        // {
-        //     print("Pickup has no audio source");
-        //     return;
-        // }
-        // audioSource.clip = clip;
-        // audioSource.loop = false;
-        // audioSource.Play();
-        
-        print("Pickup sound played");
     }
 }
