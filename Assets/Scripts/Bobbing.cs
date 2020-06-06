b6b01b22261f1464fe5713de89790ad598db9b01
Annihilator777 <66466636+Annihilator777@users.noreply.github.com>
2020-06-06 14:21:22 +0800

    level basic

---
+++ Assets/Scripts/Bobbing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class Bobbing : MonoBehaviour
+{
+    public float bobAmount = 1f;
+    public float bobSpeed = 2f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.up * (Mathf.Sin (Time.time * bobSpeed) * bobAmount);
+       
+    }
+
+}
