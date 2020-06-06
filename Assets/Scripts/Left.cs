16ce3aa3c6b35a7d5ce5d6d3fc4e5c9260c42b84
Annihilator777 <66466636+Annihilator777@users.noreply.github.com>
2020-06-06 15:31:39 +0800

    level 2

---
+++ Assets/Scripts/Left.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class Left : MonoBehaviour
+{
+    public float bobAmount = 0.05f;
+    public float bobSpeed = 0.25f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.forward* (Mathf.Sin (Time.time * bobSpeed) * bobAmount);
+       
+    }
+
+}
