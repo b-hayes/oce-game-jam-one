f28947494db104a77adbc844d73f01c87f2db5e4
b-hayes <bradozman@gmail.com>
2020-06-06 22:38:05 +1000

    added game manager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class GameManager
+{
+    public static int currentDifficulty = 0;
+    
+    public static void PlayGame(int difficulty = 0)
+    {
+        currentDifficulty = difficulty;
+        SceneManager.LoadScene(1);
+    }
+}
