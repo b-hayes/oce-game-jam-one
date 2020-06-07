d79ca040595a5934bcf285c4747addb35bfec7d3
b-hayes <bradozman@gmail.com>
2020-06-07 13:14:25 +1000

    play button

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public static class GameManager
+public class GameManager : MonoBehaviour
 {
     public static int currentDifficulty = 0;
     
